4a622aa0cc26d02ccbafb764f72ec2da5b57f28a
RomeNath <nathromero.95@gmail.com>
2019-05-31 15:28:40 -0600

    to-do

---
+++ todo.js
@@ -0,0 +1,16 @@
+<script>
+<script>
+function creli (argument) {
+    var valor = document.
+var li = document.creli.innerHTML = valor;
+var ul = document.getul.appendChild(li);
+document.getElementBy
+
+function displayDate() {
+  document.getElementById("").innerHTML = ();
+}
+
+}
+
+</script>
+
